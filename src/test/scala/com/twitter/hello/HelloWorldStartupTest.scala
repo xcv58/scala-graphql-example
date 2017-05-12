11d82b3637463120ce897db99b0c31c379f8bc02
xcv58 <i@xcv58.com>
2017-05-11 20:12:18 -0700

    Init Hello World

---
+++ src/test/scala/com/twitter/hello/HelloWorldStartupTest.scala
@@ -0,0 +1,17 @@
+package com.twitter.hello
+
+import com.google.inject.Stage
+import com.twitter.finatra.http.EmbeddedHttpServer
+import com.twitter.inject.server.FeatureTest
+
+class HelloWorldStartupTest extends FeatureTest {
+
+  override val server = new EmbeddedHttpServer(
+    twitterServer = new HelloWorldServer,
+    stage = Stage.PRODUCTION,
+    verbose = false)
+
+  test("Server#startup") {
+    server.assertHealthy()
+  }
+}
